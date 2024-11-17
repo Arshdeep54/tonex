57800f5b65c2113a5b43c7e9c47fb192f8106b45
Arshdeep54 <balarsh535@gmail.com>
2024-11-17 06:12:11 +0530

    config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
-  // Remove output: "export" as we want server-side rendering
-  images: {
-    unoptimized: true,
-  },
-  // Add proper static file handling
-};
+const nextConfig = {};
 
 export default nextConfig;
